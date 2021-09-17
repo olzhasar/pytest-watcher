cbd4feec4ae81504e039f25b9a4973ea2a4a70f7
olzhasar <o.arystanov@gmail.com>
2021-09-17 21:45:29 +0600

    Refactor code. Add tests

--- pytest_watcher/watcher.py
+++ pytest_watcher/watcher.py
@@ -1,9 +1,11 @@
 import argparse
 import subprocess
+import sys
 import threading
 import time
 from datetime import datetime, timedelta
 from pathlib import Path
+from typing import Any, Sequence, Tuple
 
 import inotify.adapters
 import inotify.constants
@@ -19,59 +21,79 @@ trigger_lock = threading.Lock()
 trigger = None
 
 
-def _is_ignored_by_git(filepath: str):
-    """Check if file is ignored by git"""
-    result = subprocess.run(["git", "check-ignore", filepath])
-    return result.returncode == 1
-
-
-def _triggers_run(filename: str, filepath: str):
+def _triggers_run(filename: str, filepath: str) -> bool:
     """
     Check if file should trigger pytest run
     """
     return filename.endswith(".py")
 
 
-def _run_pytest(args):
+def _run_pytest(args) -> None:
     subprocess.run(["pytest", *args])
 
 
-def _background_watch(path_to_watch: Path):
+def _process_event(event: Tuple[Any, Sequence[str], str, str]) -> None:
     global trigger
-    adapter = inotify.adapters.InotifyTree(path_to_watch, mask=EVENTS_TO_WATCH)
+
+    (_, type_names, filepath, filename) = event
+
+    if _triggers_run(filename, filepath):
+        with trigger_lock:
+            trigger = datetime.now()
+
+
+def _background_watch(path_to_watch: Path) -> None:
+    global trigger
+
+    _path = str(path_to_watch.absolute())
+    adapter = inotify.adapters.InotifyTree(_path, mask=EVENTS_TO_WATCH)
 
     for event in adapter.event_gen(yield_nones=False):
-        (_, type_names, filepath, filename) = event
+        _process_event(event)
 
-        if _triggers_run(filename, filepath):
 
-            with trigger_lock:
-                trigger = datetime.now()
+def _parse_arguments(args: Sequence[str]) -> Tuple[Path, float, Sequence[str]]:
+    parser = argparse.ArgumentParser(
+        prog="pytest_watcher",
+        description="""
+            Watch <path> for changes in python files and run pytest
+            if such change is detected.\n
+            Any additional arguments will be passed to pytest directly
+        """,
+    )
+    parser.add_argument("path", type=Path, help="path to watch")
+    parser.add_argument(
+        "--interval",
+        type=float,
+        default=0.5,
+        help="Watching interval in seconds (default 0.5)",
+    )
+
+    namespace, pytest_args = parser.parse_known_args(args)
+
+    return namespace.path, namespace.interval, pytest_args
+
+
+def _run_main_loop(interval: float, pytest_args: Sequence[str]) -> None:
+    global trigger
+
+    now = datetime.now()
+    if trigger and now - trigger > timedelta(seconds=interval):
+        _run_pytest(pytest_args)
+
+        with trigger_lock:
+            trigger = None
+
+    time.sleep(interval)
 
 
 def run():
-    parser = argparse.ArgumentParser(
-        usage="pytest_watcher <path> <pytest_args>",
-        description="Watch directory for changes in python files and run pytest if change detected",
-    )
-    parser.add_argument("path", type=Path, help="path to watch")
-
-    known_args, pytest_args = parser.parse_known_args()
-    path_to_watch = str(known_args.path)
+    path_to_watch, interval, pytest_args = _parse_arguments(sys.argv[1:])
 
     thread = threading.Thread(
         target=_background_watch, args=(path_to_watch,), daemon=True
     )
     thread.start()
 
-    global trigger
-
     while True:
-        now = datetime.now()
-        if trigger and now - trigger > timedelta(milliseconds=500):
-            _run_pytest(pytest_args)
-
-            with trigger_lock:
-                trigger = None
-
-        time.sleep(0.5)
+        _run_main_loop(interval, pytest_args)
--- tests/test_pytest_watcher.py
+++ tests/test_pytest_watcher.py
@@ -1,5 +1,126 @@
-from pytest_watcher import __version__
+from datetime import datetime
+from unittest.mock import MagicMock
+
+import pytest
+from freezegun import freeze_time
+from pytest_mock import MockerFixture
+from pytest_watcher import __version__, watcher
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+@pytest.fixture()
+def mock_subprocess_run(mocker: MockerFixture):
+    return mocker.patch("pytest_watcher.watcher.subprocess.run")
+
+
+@pytest.fixture(autouse=True)
+def mock_time_sleep(mocker: MockerFixture):
+    return mocker.patch("pytest_watcher.watcher.time.sleep")
+
+
+@pytest.fixture(autouse=True)
+def _release_trigger():
+    try:
+        yield
+    finally:
+        watcher.trigger = None
+
+
+@pytest.mark.parametrize(
+    ("filename", "expected"),
+    [
+        ("test.py", True),
+        ("test.pyc", False),
+        ("image.jpg", False),
+    ],
+)
+def test_triggers_run(filename, expected):
+    assert watcher._triggers_run(filename, "") == expected
+
+
+@freeze_time("2020-01-01")
+def test_process_event_trigger():
+    assert watcher.trigger is None
+
+    event = (None, [], "/home/code", "file.py")
+    watcher._process_event(event)
+
+    assert watcher.trigger == datetime(2020, 1, 1)
+
+
+@freeze_time("2020-01-01")
+def test_process_event_no_trigger():
+    assert watcher.trigger is None
+
+    event = (None, [], "/home/code", "README.md")
+    watcher._process_event(event)
+
+    assert watcher.trigger is None
+
+
+# fmt: off
+
+@pytest.mark.parametrize(
+    ("sys_args", "path_to_watch", "interval", "pytest_args"),
+    [
+        (["/home/"], "/home", 0.5, []),
+        (["/home/", "--lf", "--nf", "-x"], "/home", 0.5, ["--lf", "--nf", "-x"]),
+        ([".", "--lf", "--nf", "-x"], ".", 0.5, ["--lf", "--nf", "-x"]),
+        ([".", "--interval=0.2", "--lf", "--nf", "-x"], ".", 0.2, ["--lf", "--nf", "-x"]),
+        ([".", "--lf", "--nf", "--interval=0.3", "-x"], ".", 0.3, ["--lf", "--nf", "-x"]),
+    ],
+)
+def test_parse_arguments(sys_args, path_to_watch, interval, pytest_args):
+    _path, _interval, _pytest_args = watcher._parse_arguments(sys_args)
+
+    assert str(_path) == path_to_watch
+    assert _interval == interval
+    assert _pytest_args == pytest_args
+
+# fmt: on
+
+
+def test_run_main_loop_no_trigger(
+    mock_subprocess_run: MagicMock, mock_time_sleep: MagicMock
+):
+    watcher.trigger = None
+
+    watcher._run_main_loop(5, ["--lf"])
+
+    mock_subprocess_run.assert_not_called()
+    mock_time_sleep.assert_called_once_with(5)
+
+    assert watcher.trigger is None
+
+
+@freeze_time("2020-01-01 00:00:00")
+def test_run_main_loop_trigger_fresh(
+    mock_subprocess_run: MagicMock, mock_time_sleep: MagicMock
+):
+    watcher.trigger = datetime(2020, 1, 1, 0, 0, 0)
+
+    with freeze_time("2020-01-01 00:00:04"):
+        watcher._run_main_loop(5, ["--lf"])
+
+    mock_subprocess_run.assert_not_called()
+    mock_time_sleep.assert_called_once_with(5)
+
+    assert watcher.trigger == datetime(2020, 1, 1, 0, 0, 0)
+
+
+@freeze_time("2020-01-01 00:00:00")
+def test_run_main_loop_trigger(
+    mock_subprocess_run: MagicMock, mock_time_sleep: MagicMock
+):
+    watcher.trigger = datetime(2020, 1, 1, 0, 0, 0)
+
+    with freeze_time("2020-01-01 00:00:06"):
+        watcher._run_main_loop(5, ["--lf"])
+
+    mock_subprocess_run.assert_called_once_with(["pytest", "--lf"])
+    mock_time_sleep.assert_called_once_with(5)
+
+    assert watcher.trigger is None
