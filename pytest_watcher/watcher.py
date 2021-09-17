c7e89c6e22e894970843e83afd9301788ef7a14b
olzhasar <o.arystanov@gmail.com>
2021-09-17 13:40:09 +0600

    Run thread as a daemon. Add usage, description

--- pytest_watcher/watcher.py
+++ pytest_watcher/watcher.py
@@ -1,5 +1,4 @@
 import argparse
-import logging
 import subprocess
 import threading
 import time
@@ -9,13 +8,6 @@ from pathlib import Path
 import inotify.adapters
 import inotify.constants
 
-logger = logging.getLogger("main")
-handler = logging.FileHandler("watcher.log")
-
-logger.setLevel(logging.INFO)
-logger.addHandler(handler)
-
-
 EVENTS_TO_WATCH = (
     inotify.constants.IN_CLOSE_WRITE
     | inotify.constants.IN_CREATE
@@ -23,40 +15,53 @@ EVENTS_TO_WATCH = (
 )
 
 
+trigger_lock = threading.Lock()
+trigger = None
+
+
 def _is_ignored_by_git(filepath: str):
+    """Check if file is ignored by git"""
     result = subprocess.run(["git", "check-ignore", filepath])
     return result.returncode == 1
+
+
+def _triggers_run(filename: str, filepath: str):
+    """
+    Check if file should trigger pytest run
+    """
+    return filename.endswith(".py")
 
 
 def _run_pytest(args):
     subprocess.run(["pytest", *args])
 
 
-trigger_lock = threading.Lock()
-trigger = None
-
-
 def _background_watch(path_to_watch: Path):
-    i = inotify.adapters.InotifyTree(path_to_watch, mask=EVENTS_TO_WATCH)
+    global trigger
+    adapter = inotify.adapters.InotifyTree(path_to_watch, mask=EVENTS_TO_WATCH)
 
-    for event in i.event_gen(yield_nones=False):
-        (_, type_names, path, filename) = event
+    for event in adapter.event_gen(yield_nones=False):
+        (_, type_names, filepath, filename) = event
 
-        if filename.endswith(".py"):
-            global trigger
+        if _triggers_run(filename, filepath):
 
             with trigger_lock:
                 trigger = datetime.now()
 
 
 def run():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("path", type=Path)
+    parser = argparse.ArgumentParser(
+        usage="pytest_watcher <path> <pytest_args>",
+        description="Watch directory for changes in python files and run pytest if change detected",
+    )
+    parser.add_argument("path", type=Path, help="path to watch")
 
     known_args, pytest_args = parser.parse_known_args()
     path_to_watch = str(known_args.path)
 
-    thread = threading.Thread(target=_background_watch, args=(path_to_watch,))
+    thread = threading.Thread(
+        target=_background_watch, args=(path_to_watch,), daemon=True
+    )
     thread.start()
 
     global trigger
