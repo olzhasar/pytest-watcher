90ec1da91f426fdd11190b077b0eef3f76201304
olzhasar <o.arystanov@gmail.com>
2023-06-09 21:03:07 +0600

    Use function styled tests for config

--- tests/test_config.py
+++ tests/test_config.py
@@ -11,64 +11,64 @@ def config():
     return Config()
 
 
-class TestUpdateFromCommandLine:
-    def test_values_are_updated(self, config: Config):
-        namespace = Namespace(
-            path="custom",
-            now=True,
-            delay=20,
-            runner="tox",
-            patterns=["*.py", ".env"],
-            ignore_patterns=["main.py"],
-        )
-        runner_args = ["--lf", "--nf"]
+def test_update_from_command_line(config: Config):
+    namespace = Namespace(
+        path="custom",
+        now=True,
+        delay=20,
+        runner="tox",
+        patterns=["*.py", ".env"],
+        ignore_patterns=["main.py"],
+    )
+    runner_args = ["--lf", "--nf"]
 
-        config.update_from_command_line(namespace, runner_args)
+    config.update_from_command_line(namespace, runner_args)
 
-        for f in config.namespace_fields:
-            assert getattr(config, f) == getattr(namespace, f)
+    for f in config.namespace_fields:
+        assert getattr(config, f) == getattr(namespace, f)
 
-        assert config.runner_args == runner_args
-
-    def test_none_values_are_not_updated(self, config: Config):
-        namespace = Namespace(
-            path="custom",
-            now=None,
-            delay=None,
-            runner=None,
-            patterns=None,
-            ignore_patterns=None,
-        )
-        runner_args = None
-
-        config.update_from_command_line(namespace, runner_args)
-
-        for f in config.namespace_fields:
-            assert getattr(config, f) is not None
-
-        assert config.runner_args == []
+    assert config.runner_args == runner_args
 
 
-class TestUpdateFromConfigDict:
-    def test_values_are_updated(self, tmp_path: Path, config: Config):
-        data = {
-            "now": True,
-            "delay": 20,
-            "runner": "tox",
-            "patterns": ["*.py", ".env"],
-            "ignore_patterns": ["main.py"],
-        }
+def test_update_from_command_line_none_values(config: Config):
+    namespace = Namespace(
+        path="custom",
+        now=None,
+        delay=None,
+        runner=None,
+        patterns=None,
+        ignore_patterns=None,
+    )
+    runner_args = None
 
-        config.update_from_mapping(data)
+    config.update_from_command_line(namespace, runner_args)
 
-        for key, value in data.items():
-            assert getattr(config, key) == value
+    for f in config.namespace_fields:
+        assert getattr(config, f) is not None
 
-    def test_path_is_not_updated(self, tmp_path: Path, config: Config):
-        data = {
-            "path": "custom",
-        }
+    assert config.runner_args == []
 
-        config.update_from_mapping(data)
 
-        assert config.path != data["path"]
+def test_update_from_mapping(tmp_path: Path, config: Config):
+    data = {
+        "now": True,
+        "delay": 20,
+        "runner": "tox",
+        "patterns": ["*.py", ".env"],
+        "ignore_patterns": ["main.py"],
+    }
+
+    config.update_from_mapping(data)
+
+    for key, value in data.items():
+        assert getattr(config, key) == value
+
+
+def test_update_from_mapping_path_not_updated(tmp_path: Path, config: Config):
+    data = {
+        "path": "custom",
+    }
+
+    config.update_from_mapping(data)
+
+    assert config.path != data["path"]
